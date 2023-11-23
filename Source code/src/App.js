dda35a14abf3c7c516cee69a134771d416fa7026
KEERTHI P <124553107+Keerthi-pk10@users.noreply.github.com>
2023-11-23 20:13:35 +0530

    Update App.js

--- Source code/src/App.js
+++ Source code/src/App.js
@@ -20,25 +20,23 @@ class App extends Component {
         };
     }
   
-    // Set a user input value
     updateInput(value) {
         this.setState({
             userInput: value,
         });
     }
   
-    // Add item if user input in not empty
     addItem() {
         if (this.state.userInput !== "") {
             const userInput = {
-                // Add a random id which is used to delete
+          
                 id: Math.random(),
   
                 // Add a user value to list
                 value: this.state.userInput,
             };
   
-            // Update list
+          
             const list = [...this.state.list];
             list.push(userInput);
   
@@ -50,14 +48,13 @@ class App extends Component {
         }
     }
   
-    // Function to delete item from list use id to delete
     deleteItem(key) {
         const list = [...this.state.list];
   
-        // Filter values and leave value which we need to delete
+        
         const updateList = list.filter((item) => item.id !== key);
   
-        // Update list in state
+      
         this.setState({
             list: updateList,
         });
